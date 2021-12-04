9ddda1ddfb429323fa4eaca009443ed204ce6c0c
bzorn <v.klimenko.2137@gmail.com>
2021-12-04 20:31:31 +0300

    1

---
+++ items.py
@@ -0,0 +1,18 @@
+class Sword:
+    def __init__(self):
+        self.atk = 0
+
+
+class Armor:
+    def __init__(self):
+        self.prt = 0
+
+
+class Rune:
+    def __init__(self):
+        self.power = 0
+
+
+class ElementalCrystal:
+    def __init__(self):
+        self.power = 0
